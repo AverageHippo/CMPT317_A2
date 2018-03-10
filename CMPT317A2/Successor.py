a9ac364c3538f9127a159ab40a6d5a25848f448c
Shahriar Rahman (shr062) <shr062@mail.usask.ca>
2018-03-09 20:43:06 -0600

    Minor fixes in succ function

--- CMPT317A2/Successor.py
+++ CMPT317A2/Successor.py
@@ -6,21 +6,17 @@ def successor(state, player):
 
     player1 = ["W1", "W2", "W3", "W4", "W5"]
     player2 = ["Q", "D1", "D2", "D3"]
-
     succssor = []
 
     if (player==1):
-
         for i in player1:
             isA, x, y = B.isAlive(state, i)
             if (isA == True):
                 for r in 0,1,-1:
                     for c in 0,1,-1:
-
                         if ( (x+r) > 4 or (y+c) > 4) or ((x+r) <0  or (y+c) <0):
                             continue
                         else:
-
                             possible = B.isPossibleMove(state, i, x+r,y+c )
                             # print(possible)
                             if possible == True:
@@ -29,19 +25,15 @@ def successor(state, player):
                                 newMove = B.move(newState, i,x+r,y+c)
                                 if newMove == True:
                                     succssor.append(newState)
-
     elif(player==2):
-
         for i in player2:
             isA, x, y = B.isAlive(state, i)
             if (isA == True):
                 for r in 0,1,-1:
                     for c in 0,1,-1:
-
                         if ( (x+r) > 4 or (y+c) > 4) or ((x+r) <0  or (y+c) <0):
                             continue
                         else:
-
                             possible = B.isPossibleMove(state, i, x+r,y+c )
                             # print(possible)
                             if possible == True:
@@ -50,16 +42,18 @@ def successor(state, player):
                                 newMove = B.move(newState, i,x+r,y+c)
                                 if newMove == True:
                                     succssor.append(newState)
-
-
     else:
-        return 0
+        return []
+
 
 
 if __name__ == '__main__':
 
     New = B.create()
     B.move(New, "W1", 3,0)
-    successor(New, 2)
+    B.move(New, "W1", 2,0)
+    successor(New, 1)
+
+
 
 
